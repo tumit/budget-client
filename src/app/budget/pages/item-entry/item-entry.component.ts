e606a61aae5f6bb8c5510afdc1bf0555b1f507c9
tumit <tumit@live.com>
2024-10-12 14:47:40 +0700

    add filter

--- src/app/budget/pages/item-entry/item-entry.component.ts
+++ src/app/budget/pages/item-entry/item-entry.component.ts
@@ -1,12 +1,64 @@
+// item-entry.component.ts
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { map } from 'rxjs';
+import { Item, ItemStatus } from '../../models/item';
 
 @Component({
   selector: 'app-item-entry',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './item-entry.component.html',
   styleUrl: './item-entry.component.scss'
 })
 export class ItemEntryComponent {
+  items: Item[] = [
+    {
+      title: 'Gaming Laptop',
+      amount: 3,
+      price: 3200,
+      contactMobileNo: '0812345678',
+      status: ItemStatus.APPROVED,
+      id: 1
+    },
+    {
+      title: 'Desktop Tower',
+      amount: 7,
+      price: 2500,
+      contactMobileNo: '0823456789',
+      status: ItemStatus.PENDING,
+      id: 2
+    },
+    {
+      title: 'Mechanical Keyboard',
+      amount: 6,
+      price: 750,
+      contactMobileNo: '0834567890',
+      status: ItemStatus.REJECTED,
+      id: 3
+    }
+  ];
 
+  isSmallTable = false;
+  filterItems = this.items;
+
+  filterInput = new FormControl<string>('', { nonNullable: true });
+
+  constructor() {
+    this.filterInput.valueChanges // ดักเหตุการณ์ที่ value เปลี่ยนได้
+      .pipe(map((keyword) => keyword.toLocaleLowerCase())) // convert value ได้
+      .subscribe((keyword) => {
+        console.log('keyword', keyword)
+        this.filterItems = this.items.filter((item) => item.title.toLocaleLowerCase().includes(keyword)); // เขียน logic จากการเปลี่ยน value ได้
+      });
+
+
+    this.filterInput.valueChanges
+      .subscribe(v => console.log('v', v))
+
+
+    // Item[] => something[]
+    // this.items.map();
+
+  }
 }
